77c1c9424b602b9be5f0986d5f821bb36fe6ae41
Tiana-Finaritra <128024026+Tiana-Finaritra@users.noreply.github.com>
2024-06-03 22:04:31 +0300

    feat:contact section form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,19 @@
-'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import HeadMenu from '@/components/HeadMenu';
 import FilterSection from '@/components/FilterSection';
 import CarouselMenu from '@/components/CarouselMenu';
-import StartupPage from '../components/StartupPage';
-import AppointmentContactForm from '@/components/AppointmentContactForm';
-import SelectClient from '@/components/SelectClient';
+import ContactSection from '@/components/Contact';
+
 
 const HomePage: React.FC = () => {
-    const [showSplash, setShowSplash] = useState(true);
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowSplash(false);
-        }, 3000);
-
-        return () => clearTimeout(timer);
-    }, []);
-
-    if (showSplash) {
-        return <StartupPage />;
-    }
-
     return (
         <div>
             <HeadMenu />
-            <div style={{ display: 'flex', marginTop: '20px' }}>
+            <div style={{ display: 'flex', marginTop: '20px', flexDirection: 'row',justifyContent:'space-between' }}>
                 <FilterSection />
                 <CarouselMenu />
+                <ContactSection />
             </div>
-            <SelectClient />
-            <AppointmentContactForm />
         </div>
     );
 };
