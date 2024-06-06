8836b1d5b0cc983671dc7473f5d6951395f38642
KevinDonovan2 <hei.donovan.2@gmail.com>
2024-06-06 11:07:32 +0300

    refactor: dataProvider

--- src/components/AdminApp.tsx
+++ src/components/AdminApp.tsx
@@ -4,6 +4,10 @@ import { Admin, Resource, ListGuesser, EditGuesser } from 'react-admin';
 import authProvider from '@/providers/authProvider';
 import CustomLoginPage from '../app/login/page';
 import dataProvider from '@/providers/dataProvider';
+import UserIcon from '@mui/icons-material/Group';
+import CarIcon from '@mui/icons-material/DirectionsCar';
+import ImageIcon from '@mui/icons-material/Image';
+import AppointmentIcon from '@mui/icons-material/Event';
 
 const AdminApp: React.FC = () => (
     <Admin
@@ -11,9 +15,10 @@ const AdminApp: React.FC = () => (
         authProvider={authProvider}
         loginPage={CustomLoginPage}
     >
-        <Resource name="users" list={ListGuesser} />
-        <Resource name="cars" list={ListGuesser} edit={EditGuesser} />
-        <Resource name="comments" list={ListGuesser} edit={EditGuesser} />
+        <Resource name="users" list={ListGuesser} icon={UserIcon} />
+        <Resource name="cars" list={ListGuesser} edit={EditGuesser} icon={CarIcon} />
+        <Resource name="images" list={ListGuesser} edit={EditGuesser} icon={ImageIcon} />
+        <Resource name="appointments" list={ListGuesser} edit={EditGuesser} icon={AppointmentIcon} />
     </Admin>
 );
 
--- src/providers/dataProvider.tsx
+++ src/providers/dataProvider.tsx
@@ -1,56 +1,38 @@
 import { DataProvider } from 'react-admin';
-import { accountsProvider } from './accountsProvider';
-import { User } from './type';
+import baseProvider from './baseProvider';
+
 const dataProvider: DataProvider = {
+    ...baseProvider,
     getList: async (resource, params) => {
-        if (resource === 'users') {
-            const { data, total } = await accountsProvider.getList();
-            return {
-                data: data.map((item: any) => ({ ...item, id: item.id })),
-                total
-            };
+        if (resource === 'users' || resource === 'cars' || resource === 'images' || resource === 'appointments') {
+            return baseProvider.getList(resource, params);
         }
         return Promise.reject('Unknown resource');
     },
     getOne: async (resource, params) => {
-        if (resource === 'users') {
-            const { data } = await accountsProvider.getOne(
-                params.id.toString()
-            );
-            return { data: { ...data, id: data.id } };
+        if (resource === 'users' || resource === 'cars' || resource === 'images' || resource === 'appointments') {
+            return baseProvider.getOne(resource, params);
         }
         return Promise.reject('Unknown resource');
     },
     create: async (resource, params) => {
-        if (resource === 'users') {
-            const { data } = await accountsProvider.create(params.data as User);
-            return { data: { ...data, id: data.id } };
+        if (resource === 'users' || resource === 'cars' || resource === 'images' || resource === 'appointments') {
+            return baseProvider.create(resource, params);
         }
         return Promise.reject('Unknown resource');
     },
     update: async (resource, params) => {
-        if (resource === 'users') {
-            const { data } = await accountsProvider.update(
-                params.id,
-                params.data as User
-            );
-            return { data: { ...data, id: data.id } };
+        if (resource === 'users' || resource === 'cars' || resource === 'images' || resource === 'appointments') {
+            return baseProvider.update(resource, params);
         }
         return Promise.reject('Unknown resource');
     },
     delete: async (resource, params) => {
-        if (resource === 'users') {
-            const { data } = await accountsProvider.delete(
-                params.id.toString()
-            );
-            return { data: { ...data, id: data.id } };
+        if (resource === 'users' || resource === 'cars' || resource === 'images' || resource === 'appointments') {
+            return baseProvider.delete(resource, params);
         }
         return Promise.reject('Unknown resource');
-    },
-    deleteMany: () => Promise.reject('Not Implemented'),
-    getMany: () => Promise.reject('Not Implemented'),
-    getManyReference: () => Promise.reject('Not Implemented'),
-    updateMany: () => Promise.reject('Not Implemented')
+    }
 };
 
 export default dataProvider;
